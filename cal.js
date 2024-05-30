ec84a7abb60bba85ef2f0d3bf4547916cf25e02f
Alka Mariam Thomas <85983525+Alka555@users.noreply.github.com>
2024-05-30 15:45:05 +0530

    initial version

--- cal.js
+++ cal.js
@@ -13,6 +13,9 @@ function back(){
 function equal(){
     try{
     result.value = eval(result.value)
+    setTimeout(() => {
+        result.value = ""
+        }, 1000);
     }
     catch(error){
         result.value = 'Error'
